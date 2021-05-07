dbe7421c5c1af79e27745895411c2b4add46dc11
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-05-07 14:30:22 +0200

    Aggiunto Array di oggetti

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -8,3 +8,32 @@ for (var key in studente) {
     studente[key]
     console.log(key + ': ' + studente[key]);
 }
+
+
+var studenti = [
+    {
+        nome: 'Francesco',
+        cognome: 'Rossi',
+        età: 43
+    },
+    {
+        nome: 'Alberto',
+        cognome: 'Bianchi',
+        età: 18
+    },
+    {
+        nome: 'Franco',
+        cognome: 'Costa',
+        età: 67
+    },
+    {
+        nome: 'Andrea',
+        cognome: 'Romano',
+        età: 35
+    }
+]
+
+for(var i = 0; i < studenti.length; i++) {
+    var studEl = studenti[i];
+    console.log('Dati: ' + studEl.nome +  ' ' + studEl.cognome);
+}
